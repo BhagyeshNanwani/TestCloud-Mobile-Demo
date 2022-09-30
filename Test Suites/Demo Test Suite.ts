8ca6f83629293037cd81a2ecc558d425390c99a2
bhagyesh <bhagyesh@bhagyeshs-mbp.bbrouter>
2022-09-30 13:38:38 +0530

    Commit

---
+++ Test Suites/Demo Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>de2211c7-f6f6-40d7-a933-7f47cf8c3692</testSuiteGuid>
+</TestSuiteEntity>
